588bd610850e4f7dfa0a6c316af841f26f12e477
Buddhika Gamage <buddhikagamage1997@gmail.com>
2023-07-03 00:26:33 +0530

    another one

--- frontend/src/screens/Admin/GrnScreen.jsx
+++ frontend/src/screens/Admin/GrnScreen.jsx
@@ -17,31 +17,7 @@ import TextField from "@mui/material/TextField";
 import Autocomplete from "@mui/material/Autocomplete";
 import axios from "axios";
 
-const top100Films = [
-  { label: "The Shawshank Redemption", year: 1994 },
-  { label: "The Godfather", year: 1972 },
-  { label: "The Godfather: Part II", year: 1974 },
-  { label: "The Dark Knight", year: 2008 },
-  { label: "12 Angry Men", year: 1957 },
-  { label: "Schindler's List", year: 1993 },
-  { label: "Pulp Fiction", year: 1994 },
-  {
-    label: "The Lord of the Rings: The Return of the King",
-    year: 2003,
-  },
-  { label: "The Good, the Bad and the Ugly", year: 1966 },
-  { label: "Fight Club", year: 1999 },
-  {
-    label: "The Lord of the Rings: The Fellowship of the Ring",
-    year: 2001,
-  },
-  {
-    label: "Star Wars: Episode V - The Empire Strikes Back",
-    year: 1980,
-  },
-  { label: "Forrest Gump", year: 1994 },
-  { label: "Inception", year: 2010 },
-];
+
 
 const CreateListing = () => {
   const { id } = useParams();
@@ -78,7 +54,7 @@ const CreateListing = () => {
   useEffect(() => {
     // Fetch suggestions from the database based on the keyword
     const fetchSuggestions = async () => {
-      console.log("hi");
+
       try {
         const config = {
           headers: {
@@ -87,7 +63,6 @@ const CreateListing = () => {
         };
 
         const response = await axios.get(`/api/batches/getIds`, config);
-        console.log(response.data);
         setSuggestions(response.data);
       } catch (error) {
         // Handle fetch error
@@ -95,6 +70,8 @@ const CreateListing = () => {
     };
     fetchSuggestions();
   }, []);
+
+  
   useEffect(() => {
     dispatch(viewBatchesReset());
     if (success) {
@@ -176,11 +153,13 @@ const CreateListing = () => {
               options={suggestions}
               sx={{ width: 300 }}
               onChange={(event, value) => setKeyword(value)}
+              
               renderInput={(params) => (
                 <TextField
                   {...params}
                   required
                   className="formInputName"
+                  style={{  border: "2px solid rgb(198, 231, 198)" }}
                   sx={{
                     backgroundColor: "#ffffff",
                     width: "600px",
