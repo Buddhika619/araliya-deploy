ed0ba4753e4ac65439b91ddf62bc9283b557eb46
Buddhika Gamage <buddhikagamage1997@gmail.com>
2024-03-20 10:58:44 +0530

    fix deployment issues

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import path from "path";
 import express, { application } from "express";
 import dotenv from "dotenv";
-
+import colors from 'colors'
 import morgan from "morgan";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import connectDB from "./config/db.js";
